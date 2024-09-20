1a05410ffdbcaabb5c164c1257221565e37e1180
zainalsalamun <zainalrtf@gmail.com>
2024-09-20 11:22:18 +0700

    add language indo

---
+++ lib/language/language_in.dart
@@ -0,0 +1,16 @@
+import 'languages.dart';
+
+class LanguageIn extends Languages {
+  @override
+  String get appName => "Aplikasi Demo";
+
+  @override
+  String get welcomeText => "Selamat Datang";
+
+  @override
+  String get selectLanguage => "Pilih Bahasa Anda";
+
+  @override
+  String get appDescription =>
+      "Aplikasi ini membantu Anda dengan mudah mengimplementasikan multibahasa ke aplikasi flutter Anda.";
+}
